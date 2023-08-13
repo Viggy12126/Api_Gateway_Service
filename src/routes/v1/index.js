011c2320ff75b27a526752cd3adf5816d5ba050e
vighnesh <vighnesh2607@gmail.com>
2023-08-13 13:35:45 -0400

    added user signup api

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -2,8 +2,12 @@ const express = require('express');
 
 const { InfoController } = require('../../controllers');
 
+const userRouter = require('./user-routes');
+const user = require('../../models/user');
 const router = express.Router();
 
 router.get('/info', InfoController.info);
+
+router.use('/signup',userRouter);
 
 module.exports = router;
