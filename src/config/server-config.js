e4cfcd273c8b81f3ec98a607581f21bc4f8162f6
vighnesh <vighnesh2607@gmail.com>
2023-08-15 13:04:26 -0400

    added jwt in signin api

--- src/config/server-config.js
+++ src/config/server-config.js
@@ -3,5 +3,8 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 module.exports = {
-    PORT: process.env.PORT
-}
+    PORT: process.env.PORT,
+    SALT_ROUNDS: process.env.SALT_ROUNDS,
+    JWT_EXPIRY: process.env.JWT_EXPIRY,
+    JWT_SECRET: process.env.JWT_SECRET
+    }
--- src/utils/common/index.js
+++ src/utils/common/index.js
@@ -1,5 +1,6 @@
 module.exports = {
     ErrorResponse: require('./error-response'),
     SuccessResponse: require('./success-response'),
-    Enums: require('./enum')
+    Enums: require('./enum'),
+    Auth: require('./auth')
 }
