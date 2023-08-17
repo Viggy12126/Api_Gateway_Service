6dd197d231c5671340b8b2a270bb38f93b955ed7
vighnesh <vighnesh2607@gmail.com>
2023-08-17 18:46:20 -0400

    added rate liniter

--- src/index.js
+++ src/index.js
@@ -2,11 +2,20 @@ const express = require('express');
 
 const { ServerConfig } = require('./config');
 const apiRoutes = require('./routes');
+const rateLimit = require('express-rate-limit')
 
 const app = express();
 
+const limiter = rateLimit({
+	windowMs: 2 * 60 * 1000,
+    max:4
+	
+})
+
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+
+app.use(limiter);
 
 app.use('/api', apiRoutes);
 
