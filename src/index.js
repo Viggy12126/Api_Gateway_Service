7b70ad62341abebd242f68da85df4f549e66131d
vighnesh <vighnesh2607@gmail.com>
2023-08-19 13:31:42 -0400

    added proxy

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ const express = require('express');
 
 const { ServerConfig } = require('./config');
 const apiRoutes = require('./routes');
-const rateLimit = require('express-rate-limit')
+const rateLimit = require('express-rate-limit');
+const { createProxyMiddleware } = require('http-proxy-middleware');
 
 const app = express();
 
@@ -18,6 +19,8 @@ app.use(express.urlencoded({extended: true}));
 app.use(limiter);
 
 app.use('/api', apiRoutes);
+app.use('/flightsService',createProxyMiddleware({target:ServerConfig.FLIGHT_SERVICE,changeOrigin: true }))
+app.use('/bookingService',createProxyMiddleware({target:ServerConfig.BOOKING_SERVICE,changeOrigin: true }))
 
 app.listen(ServerConfig.PORT, () => {
     console.log(`Successfully started the server on PORT : ${ServerConfig.PORT}`);
